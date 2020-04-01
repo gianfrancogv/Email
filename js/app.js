a7a55b627bf262243a7f41a5ae0586cf9f38cb2c
Gian Franco <gianfrancogv@gmail.com>
2020-04-01 20:05:58 -0300

    Validando que los campos no esten vacios

--- js/app.js
+++ js/app.js
@@ -5,13 +5,57 @@ const mensaje = document.getElementById('mensaje');
 const btnEnviar = document.getElementById('enviar');
 
 //Event listeners
+eventListeners();
+
 function eventListeners() {
     //Inicio de la aplicacion y deshabilitar submit
     document.addEventListener('DOMContentLoaded', inicioApp);
+    //Campos del formulario
+    email.addEventListener('blur', validarCampo);
+    asunto.addEventListener('blur', validarCampo);
+    mensaje.addEventListener('blur', validarCampo);
 }
 
 //Funciones
 function inicioApp() {
     //Deshabilitar envio
     btnEnviar.disabled = true;
+}
+
+//Valida que el campo tenga algo escrito
+function validarCampo() {
+    //Se valida la longitud del texto, y que no este vacio
+    validarLongitud(this);
+    //Validar unicamente el email
+    if (this.type === 'email') {
+        validarEmail(this);
+    }
+    
+    let errores = document.querySelectorAll('.error');
+    if (email.value !== '' && asunto.value !== '' && mensaje.value !== '') {
+        if (errores.length === 0) {
+            btnEnviar.disabled = false;
+        }
+    }
+}
+
+function validarLongitud(campo) {
+    if (campo.value.length > 0) {
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+    } else {
+        campo.style.borderBottomColor = 'red';
+        campo.classList.add('error');
+    }
+}
+
+function validarEmail(campo) {
+    const mensaje = campo.value;
+    if (mensaje.indexOf('@') !== -1) {
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+    } else {
+        campo.style.borderBottomColor = 'red';
+        campo.classList.add('error');
+    }  
 }
